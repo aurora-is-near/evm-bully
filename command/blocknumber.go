994327e6bf5deccb621b9f82b0b07fb3a270efac
Frank Braun <frank@near.org>
2021-03-03 02:13:59 +0000

    add one-liner to `evm-bully blocknumber`

--- command/blocknumber.go
+++ command/blocknumber.go
@@ -14,7 +14,7 @@ func BlockNumber(argv0 string, args ...string) error {
   fs := flag.NewFlagSet(argv0, flag.ContinueOnError)
   fs.Usage = func() {
     fmt.Fprintf(os.Stderr, "Usage: %s\n", argv0)
-    fmt.Fprintf(os.Stderr, "TODO\n")
+    fmt.Fprintf(os.Stderr, "Print the current block number to stdout.\n")
     fs.PrintDefaults()
   }
   endpoint := fs.String("endpoint", defaultEndpoint, "Set JSON-RPC endpoint")
