52ab4276287f332dd218b3a0efea6261bff77264
Frank Braun <frank@near.org>
2021-04-23 01:38:38 +0000

    replayer: encode genesis account balances

--- replayer/begin_chain.go
+++ replayer/begin_chain.go
@@ -1,6 +1,7 @@
 package replayer
 
 import (
+	"encoding/binary"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -12,9 +13,31 @@ import (
 	"github.com/near/borsh-go"
 )
 
+// AccountBalance encodes (genesis) account balances used by the 'begin_chain' function.
+type AccountBalance struct {
+	Account RawAddress
+	Balance RawU256
+}
+
 // BeginChainArgs encodes the arguments for 'begin_chain'.
 type BeginChainArgs struct {
-	ChainID RawU256
+	ChainID      RawU256
+	GenesisAlloc []AccountBalance
+}
+
+func genesisAlloc(g *core.Genesis) ([]AccountBalance, error) {
+	ga := make([]AccountBalance, 0, len(g.Alloc))
+	for address, account := range g.Alloc {
+		var ab AccountBalance
+		copy(ab.Account[:], address[:])
+		b, err := bigIntToUint64(account.Balance)
+		if err != nil {
+			return nil, err
+		}
+		binary.LittleEndian.PutUint64(ab.Balance[:], b)
+		ga = append(ga, ab)
+	}
+	return ga, nil
 }
 
 func beginChain(
@@ -29,7 +52,12 @@ func beginChain(
 	fmt.Println("begin_chain()")
 
 	var args BeginChainArgs
+	var err error
 	args.ChainID[31] = chainID
+	args.GenesisAlloc, err = genesisAlloc(g)
+	if err != nil {
+		return err
+	}
 
 	data, err := borsh.Serialize(args)
 	if err != nil {
