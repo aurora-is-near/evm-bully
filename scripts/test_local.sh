834333578c8722ac26d50b7da0341325eeb95c91
Frank Braun <frank@near.org>
2021-05-07 03:01:29 +0000

    scripts: pass testnet flag to test_local.sh

--- scripts/test_local.sh
+++ scripts/test_local.sh
@@ -1,8 +1,8 @@
 #!/bin/sh -e
 
-if [ $# -ne 1 ]
+if [ $# -ne 2 ]
 then
-  echo "Usage: $0 evm.wasm" >&2
+  echo "Usage: $0 testnet_flag evm.wasm" >&2
   exit 1
 fi
 
@@ -12,6 +12,6 @@ EVM=`openssl rand -hex 16`
 export NEAR_ENV=local
 
 near create-account $EVM.$ACCOUNT --master-account=$ACCOUNT --initial-balance=1000 --keyPath=$HOME/.near/local/validator_key.json
-aurora install --chain 1313161556 --engine $EVM.$ACCOUNT --signer $EVM.$ACCOUNT --owner $EVM.$ACCOUNT $1
+aurora install --chain 1313161556 --engine $EVM.$ACCOUNT --signer $EVM.$ACCOUNT --owner $EVM.$ACCOUNT $2
 
-evm-bully -v replay -accountId $EVM.$ACCOUNT -goerli -skip $EVM.$ACCOUNT
+evm-bully -v replay -accountId $EVM.$ACCOUNT $1 -skip $EVM.$ACCOUNT
