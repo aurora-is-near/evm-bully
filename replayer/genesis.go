b914d6f7f14ee2c2f931bd380c8c89356643bc7f
Frank Braun <frank@aurora.dev>
2021-06-21 23:11:19 +0000

    improve `evm-bully genesis` output

--- replayer/genesis.go
+++ replayer/genesis.go
@@ -1,10 +1,10 @@
 package replayer
 
 import (
-	"encoding/json"
 	"fmt"
 	"sort"
 
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core"
 )
 
@@ -21,21 +21,22 @@ func getGenesisBlock(net string) *core.Genesis {
 	}
 }
 
+type AddrSlice []common.Address
+
+func (s AddrSlice) Len() int           { return len(s) }
+func (s AddrSlice) Less(i, j int) bool { return s[i].String() < s[j].String() }
+func (s AddrSlice) Swap(i, j int)      { s[i], s[j] = s[j], s[i] }
+
 func dumpAccounts(g *core.Genesis) error {
-	var addresses []string
-	accounts := make(map[string]string)
+	var addresses AddrSlice
+	accounts := make(map[common.Address]string)
 	for address, account := range g.Alloc {
-		a := address.String()
-		addresses = append(addresses, a)
-		jsn, err := json.MarshalIndent(account, "", "  ")
-		if err != nil {
-			return err
-		}
-		accounts[a] = string(jsn)
+		addresses = append(addresses, address)
+		accounts[address] = account.Balance.String()
 	}
-	sort.Strings(addresses)
+	sort.Sort(addresses)
 	for _, a := range addresses {
-		fmt.Printf("%s: %s\n", a, accounts[a])
+		fmt.Printf("%s: %s\n", a.String(), accounts[a])
 	}
 	return nil
 }
